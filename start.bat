2af5a43957018ed2959b11cc48d93c6ccd6b112d
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-07-25 17:02:12 -0300

    start.bat and start.sh

---
+++ start.bat
@@ -0,0 +1,38 @@
+﻿@echo off
+title Uork - API
+setlocal
+
+echo Uork - API
+echo 1 - Consultar Status de um serviço
+echo 2 - Consultar Conta
+echo 3 - Consultar Notícias
+echo 4 - Instalar Dependências
+
+set /p option=Qual a escolha desejada: 
+
+if "%option%"=="1" (
+   cls
+   npm run status
+   timeout /t 60
+  
+) else if "%option%"=="2" (
+  cls
+  npm run conta
+  timeout /t 60
+ 
+) else if "%option%"=="3" (
+  cls
+  npm run noticias
+  timeout /t 60
+ 
+) else if "%option%"=="4" (
+  cls
+  npm install
+) else (
+    echo Opção inválida.
+    timeout /t 60
+                      
+
+)
+
+pause
---
+++ start.sh
@@ -0,0 +1,28 @@
+﻿#!/bin/bash
+echo Uork - API
+echo 1 - Consultar Status de um serviço
+echo 2 - Consultar Conta
+echo 3 - Consultar Notícias
+echo 4 - Instalar Dependências
+
+read -p "Qual a escolha desejada: " option
+
+if [ "$option" -eq 1 ]; then
+   clear
+   npm run status
+   read -p "Pressione Enter para continuar..."
+elif [ "$option" -eq 2 ]; then
+   clear
+   npm run conta
+   read -p "Pressione Enter para continuar..."
+elif [ "$option" -eq 3 ]; then
+   clear
+   npm run noticias
+   read -p "Pressione Enter para continuar..."
+elif [ "$option" -eq 4 ]; then
+   clear
+   npm install
+else
+   echo "Opção inválida."
+fi
+
